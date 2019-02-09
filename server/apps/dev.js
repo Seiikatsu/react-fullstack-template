5b4fa3fb90dd43c6c621929619a16503a72c9330
seikatsu <seikatsu@seikatsu.io>
2019-02-09 14:55:16 +0100

    comments, removed unused

--- server/apps/dev.js
+++ server/apps/dev.js
@@ -4,10 +4,16 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 
+/*
+* In development, we dont care about the GET route "/", we only provide routes that we need for our api to the client.
+*/
 app.get('/', (req, res) => {
     res.send('You wont find anything here in development mode!');
 });
 
+/*
+* Dummy POST route that could be removed later and is only used for testing.
+*/
 app.post('/ping', (req, res) => {
     console.log('PING');
     res.send({
